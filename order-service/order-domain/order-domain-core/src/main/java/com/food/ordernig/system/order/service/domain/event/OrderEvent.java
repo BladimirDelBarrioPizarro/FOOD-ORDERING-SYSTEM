9fcf66f2a29b195c27c7cf39b250b0090146fc10
Bladi <bladimaltego@gmail.com>
2025-01-26 12:42:26 +0100

    feat: updated OrderEvent

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordernig/system/order/service/domain/event/OrderEvent.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordernig/system/order/service/domain/event/OrderEvent.java
@@ -2,12 +2,24 @@ package com.food.ordernig.system.order.service.domain.event;
 
 import com.food.ordering.system.domain.event.DomainEvent;
 import com.food.ordernig.system.order.service.domain.entity.Order;
-import lombok.Data;
+
 
 import java.time.ZonedDateTime;
 
-@Data
 public abstract class OrderEvent implements DomainEvent<Order> {
     private final Order order;
     private final ZonedDateTime createdAt;
+
+    public OrderEvent(Order order, ZonedDateTime createdAt) {
+        this.order = order;
+        this.createdAt = createdAt;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
 }
